2e491affadc14a06f40d4c8777f5643baa039dd9
Roshan Prabhakar <roshan@aprabhakar.com>
2019-02-20 20:11:01 -0800

    fixed alligning for sergio div

--- acclaims.js
+++
@@ -1,8 +0,0 @@
-function start_scroll_down() {
-    scroll = setInterval(function(){ window.scrollBy(0, 1000); console.log('start');}, 1500);
-}
-
-function stop_scroll_down() {
-    clearInterval(scroll);
-    console.log('stop');
-}
